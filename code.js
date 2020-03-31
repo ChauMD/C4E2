168cef934176fbbd88107ece3e6539f393e0e8b5
ChauMD <chauhoangleminh@gmail.com>
2020-04-01 03:57:10 +0700

    change mix

--- code.js
+++ code.js
@@ -7,9 +7,11 @@ const TIMER = {
     BREAK_LENGTHconst: 5,
     SESSION_LENGTH: 25,
 };
+// Goi bien 
 let breakLength = 5; 
 let sessionLength = 25;
 let time = '25:00';
+let end = '00:00';
 // let remainingtimes = sessionLength*60;
 let model = STATES.SESSION; //(session.break)//
 let ISP = STATES.ISP;
@@ -79,7 +81,20 @@ function countdown(initseconds) {
           break;
           default:
     };
-  }
+  };
+//   function a()  {
+//       x = setInterval(function (){
+//           now = $.now();
+//           remainingtimes = end - now;
+//           minutes = Math.floor((remainingtimes % (1000*60*60))/(1000*60));
+//           seconds = Math.floor((remainingtimes % (1000*60)/1000));
+//           if (seconds == 60) {
+//               document.getElementById('display').innerHTML='1:00';
+//           } else if (seconds < 10) {
+//               document.getElementById('display').innerHTML= minutes + ':0' + seconds;
+//           } else document.getElementById('display').innerHTML = minutes + ':' + seconds;
+//       })
+//   }
 
 
 // function countdown() {
